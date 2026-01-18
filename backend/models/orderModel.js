58effb610b8e82993cded2318d554993fcada736
Abhishek <ytabhishek721@gmail.com>
2026-01-18 22:14:44 +0530

    Update CSS styles

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,7 +2,38 @@
 
 
 
-// models/orderModel.js
+// // models/orderModel.js
+// import mongoose from "mongoose";
+
+// const orderSchema = new mongoose.Schema({
+//   dealerId: { 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: "Dealer" 
+//   },
+//   address: String,
+
+//   items: [
+//     {
+//       productId: String, // ✅ String type (numeric ya alphanumeric dono accept karega)
+//       name: String,
+//       price: Number,
+//       qty: Number
+//     }
+//   ],
+
+//   totalAmount: Number,
+//   createdAt: { 
+//     type: Date, 
+//     default: Date.now 
+//   }
+// });
+
+// export default mongoose.model("Order", orderSchema);
+
+
+
+
+
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
@@ -14,10 +45,12 @@ const orderSchema = new mongoose.Schema({
 
   items: [
     {
-      productId: String, // ✅ String type (numeric ya alphanumeric dono accept karega)
+      productId: String,
       name: String,
       price: Number,
-      qty: Number
+      qty: Number,
+      weight: String,  // ✅ ADD THIS LINE
+      basePrice: Number // Optional: यदि चाहें तो
     }
   ],
 
